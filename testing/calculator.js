499620e6b88db4efb5bee73144c3d479079503bd
Henrique Heron <henriqueheron1@gmail.com>
2024-03-07 21:27:05 -0300

    add tests to calculator.js

--- testing/calculator.js
+++ testing/calculator.js
@@ -1,15 +1,23 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  if (b === 0) throw new Error('Cannot divide by zero');
+  return a / b;
 }
+
+module.exports = {
+  sum: sum,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
+};
--- testing/calculator.test.js
+++ testing/calculator.test.js
@@ -1,5 +1,31 @@
-const calculator = require('./calculator')
+const { sum, subtract, multiply, divide } = require('./calculator');
 
-it('Sums values', () => {
-    expect()
-})
+it('Sums two values', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+it('Subtracts two values', () => {
+  expect(subtract(14, 3)).toBe(11);
+});
+
+it('Subtracts two value such that the result is negative', () => {
+  expect(subtract(-133, 5)).toBe(-138);
+});
+
+it('Multiplies two values', () => {
+  expect(multiply(2, 3)).toBe(6);
+});
+
+it('Multiplies by a non-number', () => {
+  expect(multiply(10, 'cool')).toBeNaN();
+});
+
+it('Divides two numbers', () => {
+  expect(divide(2, 3)).toBeLessThan(1);
+});
+
+it('Divides a number by zero', () => {
+  expect(() => {
+    divide(14, 0);
+  }).toThrow('Cannot divide by zero');
+});
